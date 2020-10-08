40670b3b02f368276a2e2f5ab977171f4abe10b7
Mvb1122 <68322167+Mvb1122@users.noreply.github.com>
2020-10-07 21:46:55 -0600

    started putting commands in one thing
    gonna rewrite it all into one for latency later

--- bot.js
+++ bot.js
@@ -1,7 +1,10 @@
 // this loads the config file which tells it what to connect to.
 const config = require('./config.json');
+// this loads the 8ball.js file which has the 8ball command I made.
+var eball = require('./Command_Files/8ball');
 // this makes the program a discord bot
 const Discord = require('discord.js');
+// this bit lets me log to the command console.
 const { Console } = require('winston/lib/winston/transports');
 
 // create a new Discord client
@@ -11,51 +14,53 @@ const client = new Discord.Client();
 // (this will only trigger one time after logging in)
 client.once('ready', () => {
 	console.log('Ready!');
+	channel = client.channels.cache.get('762868747970936882');
+    channel.send('Bot updated.');
 });
 
 // This lil buckaroonie sends Boo when you ask to be spooked.
-client.on('message', msg => {
- if (msg.content === 'h!spook') {
+client.on('message', message => {
+ if (message.content === 'h!spook') {
  // This is basically looking at every message sent in the server and looking for "h!spook"
  msg.reply('Boo');
  // then it's just sending back "Boo"
  }
 });
  
-// If you put the same input/output phrase you get a spam bot, therefore you have
-// to make it different. You can do this through any way at all, EXCLUDING
-// putting a space after the message, because discord will trim it off.
-client.on("message", (message) => {
-	if (message.content == "glizzy") {
-	  message.channel.send("Glizzy");
-	}
-});
-
 //  hello jazon
-client.on('message', msg => {
- if (msg.content === 'hello jazon') {
- msg.reply('hello jazon');
+client.on('message', message => {
+ if (message.content === 'hello jazon') {
+ msg.reply('Hello Jazon');
  }
 });
 
 // This is a different version of the "hello jazon" command. It is just a bit proper-er.
 client.on("message", (message) => {
 	if (message.content == "hello jazon") {
-	  message.channel.send("_hello jazon");
+	  msgchannel.send("_hello jazon");
 	}
 });
 
 // there are a few more dumb ones here.
 client.on("message", (message) => {
 	if (message.content == "Should I add Michelle Dominguez to the cult?") {
-	  message.channel.send("yes");
+	  msgchannel.send("yes");
+	}
+});
+
+// If you put the same input/output phrase you get a spam bot, therefore you have
+// to make it different. You can do this through any way at all, EXCLUDING
+// putting a space after the message, because discord will trim it off.
+client.on("message", (message) => {
+	if (message.content == "glizzy") {
+	  msgchannel.send("Glizzy");
 	}
 });
 
 // last one.
 client.on("message", (message) => {
 	if (message.content == "Hello") {
-	  message.channel.send("Gamers");
+	  msgchannel.send("Gamers");
 	}
 });
 
@@ -64,11 +69,11 @@ client.on("message", (message) => {
 client.on("message", (message) => {
 	if (!message.content.startsWith("I'm") || message.author.bot) return;
  // This part                         ^ tells us what to look for.
- // then the bot takes everything else, and puts it in a message.
+ // then the bot takes everything else, and puts it in a msg
 	const args = message.content.slice(3).trim().split(' ');
  //                                    ^ how wide the phrase to look for is
 	const command = args.shift().toLowerCase();
-	message.channel.send("Hello " + message.content + " I'm dad.")
+	message.channel.send("Hello " + message.content.slice(4) + " I'm dad.")
  //                       ^ reply to message with whole message, append "I'm dad"
 });
 
@@ -78,13 +83,13 @@ client.on("message", (message) => {
 /*
 client.on("message", (message) => {
 	if (message.content == "I'm a cool and good programmer") {
-	  message.channel.send("no");
+	  msgchannel.send("no");
 	}
 });
 
 client.on("message", (message) => {
 	if (message.content == "):") {
-	  message.channel.send("suck it");
+	  msgchannel.send("suck it");
 	}
 });
 */
@@ -92,10 +97,23 @@ client.on("message", (message) => {
 	if (message.content == "h!status-streaming") {
  // All this does is make the bot look nice and active.
 		console.log("Change Status to Online Logged.")
-		client.user.setActivity("I'm a spooky bot.", {
+		client.user.setActivity("h!help LIVE ON TWITCH! ! ! ", {
 			type: "STREAMING",
 			url: "https://twitch.tv/DylamLIVE"
 		  });
+		msgchannel.send("Status Changed.")
+	}
+});
+
+client.on("message", (message) => {
+	if (message.content == "h!status-streaming2") {
+ // All this does is make the bot look nice and active.
+		console.log("Change Status to Online Logged.")
+		client.user.setActivity("I'm a spooky bot.", {
+			type: "STREAMING",
+			url: config.twitchurl
+		  });
+		msgchannel.send("Status Changed.")
 	}
 });
 
@@ -106,11 +124,11 @@ client.on("message", (message) => {
 client.on("message", (message) => {
 	if (message.content == config.prefix + "status-offline") {
  // All this does is make the bot look nice and dead.
+ // Bots can't go invis, they only have online, streaming, dnd and idle.
+ // (Or off)
 		console.log("Change Status to Offline Logged.")
-		client.user.setActivity("I'm a spooky bot.", {
-			type: "INVISIBLE",
-			url: config.twitchurl
-		  });
+		client.user.setActivity("h!help")
+		msgchannel.send("Status Changed.")
 	}
 });
 
@@ -119,10 +137,23 @@ client.on("message", (message) => {
 	if (message.content == config.prefix + "status-dnd") {
  // All this does is make the bot look nice and dead.
 		console.log("Change Status to Do-Not-Disturb Logged.")
-		client.user.setActivity("I'm a spooky bot.", {
+		client.user.setActivity("h!help", {
 			type: "dnd",
 			url: config.twitchurl
 		  });
+		msgchannel.send("Status Changed.")
+	}
+});
+
+client.on("message", (message) => {
+	if (message.content == config.prefix + "status-online") {
+ // All this does is make the bot look nice and dead.
+		console.log("Change Status to norm Logged.")
+		client.user.setActivity("h!help", {
+			type: "dnd",
+			url: config.twitchurl
+		  });
+		msgchannel.send("Status Changed.")
 	}
 });
 
@@ -131,10 +162,63 @@ client.on("message", (message) => {
 	var x = Math.floor(Math.random()*(config.Nmax-config.Nmin+1)+config.Nmin);
 //                                     ^ anytime you^ see config.* you can check config.json for the thing.
 	if (message.content == config.prefix + "randomN") {
-		message.channel.send("https://nhentai.net/g/" + x );
+		msgchannel.send("https://nhentai.net/g/" + x );
 		console.log("Somebody's horney. They're looking at "+ x + ", I think.")
 	}
 });
-	
+
+client.on("message", (message) => {
+	if (message.content == "Can i kick ishaan") {
+	  msgchannel.send("Go ahead.");
+	}
+});
+
+// another way to avoid spam is to make it a code line in discord with these things -> `
+client.on("message", (message) => {
+	if (message.content == "anyway") {
+	  message.channel.send("`anyway`");
+	}
+});
+client.on("message", (message) => {
+	if (message.content == "!knock") {
+	  msgchannel.send("<@!462643693980221441> I'm at your door, let me in or you die.");
+	} //                          ^ This is greg's id, please don't annoy him.
+});
+/*
+// send DMs from bot test. Going to rework this into a help command.
+client.on('message', msg => {    
+	if (message.content === config.prefix + "dmtest") {      
+	  msg.channel.type === (`"dm"`) + msg.author.send(`wow look at that it worked.`);
+	  console.log("DM sent.")
+	  // swapping to these lil things ` <- because they let me use " in my msg.
+	}
+});
+*/
+client.on('message', msg => {    
+	if (msg.content === config.prefix + "help") {      
+		msg.channel.type === (`"dm"`) + msg.author.send({embed: {
+			color: 000000,
+			title: ("Commands:"),
+			fields: [
+			  { name: "Input", value: "randomN\nDad bot rip-off\n8ball", inline: true},
+			  { name: "Result", value: "Sends a random Nhentai\nread the title\nreads what you sent, predicts what will happen.", inline: true}
+			]}			
+		})
+		msg.channel.send("Check your dms.")
+	// This is known as chaining your commands to make it more efficient and faster to start.
+	// the bot takes roughly 11 seconds to start, so I have to make preformance 
+	// optimizations.
+	} 
+	if (msg.content.startsWith(config.prefix + "8ball")) {
+		console.log("8ball requested.")
+		var question = msg.content.slice(config.prefix.length + "8ball".length, 0);
+		// the command above cuts it up, below sends it through and replies.
+		msg.channel.send(eball.eball("", question))
+	}
+	if (msg.content.startsWith("Anyway")) {
+		msg.channel.send("`Anyway`")
+	}
+});
+
 // this wee bit logs the bot in
 client.login(config.token);
