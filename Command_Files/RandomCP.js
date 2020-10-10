7a5ccdc759aee638955570a0ee9f6651fe5e8f40
Mb1122 <micahbushman.school@gmail.com>
2020-10-09 20:19:15 -0600

    Added 7 CreepyPastas
    Thanks to the ppl who sent me their favs.

--- Command_Files/RandomCP.js
+++ Command_Files/RandomCP.js
@@ -5,11 +5,8 @@ const Discord = require('discord.js');
 const { Console } = require('winston/lib/winston/transports');
 
 exports.randomCP = () => {
-  // const userName = username;
-  // userName ? console.log("Hello, " + userName + ".") : console.log("Hello!");
-  // const userQuestion = question;
-  // userName ? console.log("You asked '" + userQuestion + "', right, " + userName + "?") : console.log("You asked '" + userQuestion + "' right?");
-  let randomNumber = Math.floor(Math.random() * 8);
+  // it's of note that I didn't know this would be so long. I'd've used an array if I knew. Welp, more to the pile to rewrite.
+  let randomNumber = Math.floor(Math.random() * 9);
   var output = "";
   switch (randomNumber) {
     case 1:
@@ -19,22 +16,25 @@ exports.randomCP = () => {
       output = "https://www.youtube.com/watch?v=6Tqfd6Z1qL4";
       break;
     case 3:
-      output = "3";
+      output = "https://www.youtube.com/watch?v=pySDIX_rIIg";
       break;
     case 4:
-      output = "4";
+      output = "https://www.youtube.com/watch?v=reM-dfNNpnw";
       break;
     case 5:
-      output = "5";
+      output = "https://youtu.be/St2crus4nFI";
       break;
     case 6:
-      output = "6";
+      output = "https://youtu.be/KHseQgE9Ew4";
       break;
     case 7:
-      output = "7";
+      output = "https://youtu.be/LKfnrE9KBW8";
       break;
     case 8:
-      output = "8";
+      output = "https://youtu.be/ZRegUjD-F-Y";
+      break;
+    case 9:
+      output = "https://youtu.be/lE_18s2Wu4U";
       break;
   };
   return output;
