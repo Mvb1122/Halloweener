ca814c4f7197ba8fe3d5d46d8cdaf9aa641e5c0a
Mb1122 <micahbushman.school@gmail.com>
2020-10-13 07:42:54 -0600

    Jazon wanted this added in.

--- Jazon_Files/3switch.js
+++ Jazon_Files/3switch.js
@@ -5,53 +5,61 @@ exports.cockrater = (input) => {
     let girthOut
     let lengthOut
     let hairOut
-      // SOLUTION: Write a looping nested loop that checks if input === short normal whatever. input is an array.
-    switch(cockLength) {
-  case 'short': {
+    let cockLength = input[0]
+    let cockGirth = input[1]
+    let cockHair = input[2]
+    console.log(`1 ${input[1]} 2 ${input[2]} 3 ${input[3]}`);
+    console.log(input)
+  switch(cockLength) {
+   case 'short': {
    lengthOut = 'Sorry man, that cock is SUPER short. Late bloomer huh?'
    break
-  }
-  case 'normal': {
+   }
+   case 'normal': {
    lengthOut = 'Cool cock, not too big, not too short.'
    break
-  }
-  case 'long': {
+   }
+   case 'long': {
    lengthOut = 'God DAMN you could play jumprope with that thing!'
    break
-  }
-  default: {
+   } 
+   default: {
    lengthOut = "Bruh you don't even got a cock"
+   }
   }
- }
-    switch(cockGirth) {
- case 'thin': {
-  girthOut = 'That a spaghetti noodle, or a cock?'
-  break
- }
- case 'average': {
-  girthOut = 'Not too skinny, not too fat, pretty nice.'
-  break
- }
- case 'fat': {
-  girthOut = 'Holy shit, is that a beer can?'
-  break
- }
- default: {
-  girthOut = 'Again, no cock... how can a non-cock user have girth? Idiot.'
- }
- }
-    switch(cockHair) {
-  case 'trimmed':
-   hairOut = 'Good job keeping the lawn clean!'
+   switch(cockGirth) {
+   case 'thin': {
+   girthOut = 'That a spaghetti noodle, or a cock?'
    break
-  case 'short':
+  }
+   case 'average': {
+   girthOut = 'Not too skinny, not too fat, pretty nice.'
+   break
+  }
+   case 'fat': {
+   girthOut = 'Holy shit, is that a beer can?'
+   break
+  }
+   default: {
+   girthOut = 'Again, no cock... how can a non-cock user have girth? Idiot.'
+  } 
+  }
+  switch(cockHair) {
+   case 'trimmed': {
+    hairOut = 'Good job keeping the lawn clean!'
+    break
+   }
+   case 'short': {
    hairOut = 'Could use some trimming, but not too bad.'
    break
-  case 'bush':
+   }
+   case 'bush': {
    hairOut = "Oh my GOD, you're gonna need a damn weedwacker for that!"
    break
-  default:
+   }
+   default: {
    hairOut = "No cock, no bush, it's not hard!"
-    }
-    return (`Here's your weenie rating: \n Hair: ${hairOut} \n Girth: ${girthOut} \n Length: ${lengthOut}`)
+   }
+  }
+return (`Here's your weenie rating: \n Hair: ${hairOut} \n Girth: ${girthOut} \n Length: ${lengthOut}`)
 }
