c826554d96a7c4dd587efb39dc35f8abcfbad3ab
Mvb1122 <68322167+Mvb1122@users.noreply.github.com>
2020-10-14 16:56:59 -0600

    My god github why do you do this
    ouch oof bang bang

--- Jazon_Files/3switch.js
+++ Jazon_Files/3switch.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 exports.cockrater = (input) => {
     // short thin trimmed
     input = input.toLowerCase()
@@ -61,70 +60,4 @@ exports.cockrater = (input) => {
    }
   }
 return (`Here's your weenie rating: \n Hair: ${hairOut} \n Girth: ${girthOut} \n Length: ${lengthOut}`)
-=======
-exports.cockrater = (input) => {
-    // short thin trimmed
-    input = input.toLowerCase()
-    input = input.trim().split(" ");
-    let girthOut
-    let lengthOut
-    let hairOut
-    let cockLength = input[0]
-    let cockGirth = input[1]
-    let cockHair = input[2]
-    console.log(`1 ${input[1]} 2 ${input[2]} 3 ${input[3]}`);
-    console.log(input)
-  switch(cockLength) {
-   case 'short': {
-   lengthOut = 'Sorry man, that cock is SUPER short. Late bloomer huh?'
-   break
-   }
-   case 'normal': {
-   lengthOut = 'Cool cock, not too big, not too short.'
-   break
-   }
-   case 'long': {
-   lengthOut = 'God DAMN you could play jumprope with that thing!'
-   break
-   } 
-   default: {
-   lengthOut = "Bruh you don't even got a cock"
-   }
-  }
-   switch(cockGirth) {
-   case 'thin': {
-   girthOut = 'That a spaghetti noodle, or a cock?'
-   break
-  }
-   case 'average': {
-   girthOut = 'Not too skinny, not too fat, pretty nice.'
-   break
-  }
-   case 'fat': {
-   girthOut = 'Holy shit, is that a beer can?'
-   break
-  }
-   default: {
-   girthOut = 'Again, no cock... how can a non-cock user have girth? Idiot.'
-  } 
-  }
-  switch(cockHair) {
-   case 'trimmed': {
-    hairOut = 'Good job keeping the lawn clean!'
-    break
-   }
-   case 'short': {
-   hairOut = 'Could use some trimming, but not too bad.'
-   break
-   }
-   case 'bush': {
-   hairOut = "Oh my GOD, you're gonna need a damn weedwacker for that!"
-   break
-   }
-   default: {
-   hairOut = "No cock, no bush, it's not hard!"
-   }
-  }
-return (`Here's your weenie rating: \n Hair: ${hairOut} \n Girth: ${girthOut} \n Length: ${lengthOut}`)
->>>>>>> d951dce2817ed47190fce65f4c73f54b2dda7790
 }
